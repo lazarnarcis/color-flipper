8337384a7f16dc8c3c14ea184b985e57f1e811f4
Narcis <82839799+narcis340@users.noreply.github.com>
2021-07-04 20:35:46 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,11 +3,19 @@ function generateColor() {
     let x = Math.floor((Math.random() * 256) + 1);
     let y = Math.floor((Math.random() * 256) + 1);
     let z = Math.floor((Math.random() * 256) + 1);
+    let r = toHex(x);
+    let g = toHex(y);
+    let b = toHex(z);
     document.body.style.backgroundColor = "rgb(" + x + ", " + y + ", " + z + ")";
-    text.innerHTML = "Background generated: rgb(" + x + ", " + y + ", " + z + ")";
+    text.innerHTML = "Background generated: rgb(" + x + ", " + y + ", " + z + ")<br/>Hex: #" + r + g + b;
 }
 generateColor();
 let button = document.getElementById("button");
 button.addEventListener("click", function() {
     generateColor();
 });
+function toHex(n) {
+  var hex = n.toString(16);
+  while (hex.length < 2) { hex = "0" + hex; }
+  return hex;
+}
